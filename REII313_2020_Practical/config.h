e35cfbd72cb5a9d0e9c8d662d7138cb2f2730988
JanicoSnyman <janicosnyman@gmail.com>
2020-04-16 14:23:54 +0200

    Initial layout of task bar and buttons

--- REII313_2020_Practical/config.h
+++ REII313_2020_Practical/config.h
@@ -4,4 +4,7 @@
 #define sceneWidth 1000
 #define sceneHeight 700
 
+#define taskbar 50
+#define padding 5
+
 #endif // CONFIG_H
--- REII313_2020_Practical/environment.cpp
+++ REII313_2020_Practical/environment.cpp
@@ -1,5 +1,9 @@
 #include "environment.h"
 
 Environment::Environment() {
-
+    this->addRect(0, 0, sceneWidth, taskbar);
+    this->addRect(padding, padding, taskbar - 2*padding, taskbar - 2*padding);
+    this->addRect(padding + taskbar, padding, taskbar - 2*padding, taskbar - 2*padding);
+    this->addRect(padding + 2*taskbar, padding, taskbar - 2*padding, taskbar - 2*padding);
+    this->addRect(padding + 3*taskbar, padding, taskbar - 2*padding, taskbar - 2*padding);
 }
